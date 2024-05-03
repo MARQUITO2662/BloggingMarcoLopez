2ac4bf9f757f326626931774bc338f581eb737bf
marco <marquito2662@gmIL.com>
2024-05-02 20:34:43 -0500

    carpetal yml eliminada

--- src/swaggerUi.js
+++ src/swaggerUi.js
@@ -2,9 +2,3 @@ import { serve, setup } from 'swagger-ui-express'
 import swaggerDocument from './swagger-output.json' assert{type:'json'}
 export const swaggerDocs = (app) => app.use('/api-docs', serve, setup(swaggerDocument))  
 
-/* import { serve, setup } from 'swagger-ui-express'
-import swaggerDocument from './swagger-output.json'
-
-export const swaggerDocs = (app) => {
-  app.use('/api-docs', serve, setup(swaggerDocument))
-} */
